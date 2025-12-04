a2b24ec718029efe1b89b0194a350b798cef895c
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2025-12-04 09:59:45 +0100

    add template

---
+++ crates/template/src/main.rs
@@ -0,0 +1,19 @@
+fn part1(input: &str) -> usize {
+    0
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    assert_eq!(part1(include_str!("./input_test.txt")), 0);
+    //     assert_eq!(part1(include_str!("./input.txt")), 0);
+    //     assert_eq!(part2(include_str!("./input_test.txt")), 0);
+    //     assert_eq!(part2(include_str!("./input.txt")), 0);
+}
+
+#[test]
+fn test() {
+    main();
+}
