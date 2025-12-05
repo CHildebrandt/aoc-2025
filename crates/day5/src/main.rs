ea49618baf44eef1b398f772b06870f2c18c30e4
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2025-12-05 11:21:11 +0100

    day5 part2

--- crates/day5/src/main.rs
+++ crates/day5/src/main.rs
@@ -17,14 +17,49 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut ranges = utils::split_double_newline_once(input)
+        .0
+        .lines()
+        .map(|line| line.split_once("-").unwrap())
+        .map(|range| {
+            (
+                range.0.parse::<usize>().unwrap(),
+                range.1.parse::<usize>().unwrap(),
+            )
+        })
+        .collect::<Vec<_>>();
+    ranges.sort();
+    ranges
+        .iter()
+        .fold(Vec::<(usize, usize)>::new(), |mut list, curr| {
+            for range in list.iter_mut() {
+                if range.0 <= curr.0 && range.1 >= curr.1 {
+                    return list;
+                }
+                // Overlapping start
+                if curr.0 < range.0 && curr.1 >= range.0 && curr.1 <= range.1 {
+                    range.0 = curr.0;
+                    return list;
+                }
+                // Overlapping end
+                if curr.1 > range.1 && range.1 >= curr.0 {
+                    range.1 = curr.1;
+                    return list;
+                }
+            }
+            list.push(*curr);
+            list
+        })
+        .iter()
+        .map(|v| v.1 - v.0 + 1)
+        .sum()
 }
 
 fn main() {
     assert_eq!(part1(include_str!("./input_test.txt")), 3);
     assert_eq!(part1(include_str!("./input.txt")), 896);
-    //     assert_eq!(part2(include_str!("./input_test.txt")), 0);
-    //     assert_eq!(part2(include_str!("./input.txt")), 0);
+    assert_eq!(part2(include_str!("./input_test.txt")), 14);
+    assert_eq!(part2(include_str!("./input.txt")), 346240317247002);
 }
 
 #[test]
