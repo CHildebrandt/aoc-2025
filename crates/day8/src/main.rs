7eaf8f9700a20d3c8d0075592d241a4d8d8b5e60
Christoffer Hildebrandt <christoffer.hildebrandt@gmail.com>
2025-12-08 20:30:12 +0100

    day8 part 2

--- crates/day8/src/main.rs
+++ crates/day8/src/main.rs
@@ -34,14 +34,11 @@ fn part1(input: &str, max: usize) -> usize {
         .map(Point::from_str)
         .tuple_combinations()
         .fold(HashMap::new(), |mut acc, (p1, p2)| {
-            acc.insert(
-                (p1.min(p2).clone(), p1.max(p2).clone()),
-                p1.straight_line_distance(&p2),
-            );
+            acc.insert((p1.min(p2), p1.max(p2)), p1.straight_line_distance(&p2));
             acc
         })
-        .iter()
-        .map(|((a, b), distance)| (a.clone(), b.clone(), *distance))
+        .into_iter()
+        .map(|((a, b), distance)| (a, b, distance))
         .collect::<Vec<_>>();
     mappings.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap());
     let mut sets = Vec::<HashSet<Point>>::new();
@@ -74,14 +71,57 @@ fn part1(input: &str, max: usize) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let num_points = input.lines().count();
+    let mut mappings = input
+        .lines()
+        .map(Point::from_str)
+        .tuple_combinations()
+        .fold(HashMap::new(), |mut acc, (p1, p2)| {
+            acc.insert((p1.min(p2), p1.max(p2)), p1.straight_line_distance(&p2));
+            acc
+        })
+        .into_iter()
+        .map(|((a, b), distance)| (a, b, distance))
+        .collect::<Vec<_>>();
+    mappings.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap());
+    let mut sets = Vec::<HashSet<Point>>::new();
+    let mut last_join: Option<(Point, Point)> = None;
+    for (a, b, _) in mappings.iter() {
+        let a_idx = sets.iter().position(|s| s.contains(a));
+        let b_idx = sets.iter().position(|s| s.contains(b));
+
+        match (a_idx, b_idx) {
+            (Some(i), Some(j)) if i != j => {
+                let set_b = sets.remove(j.max(i));
+                sets[j.min(i)].extend(set_b);
+            }
+            (Some(i), None) => {
+                sets[i].insert(b.clone());
+            }
+            (None, Some(j)) => {
+                sets[j].insert(a.clone());
+            }
+            (None, None) => {
+                let mut new_set = HashSet::new();
+                new_set.insert(a.clone());
+                new_set.insert(b.clone());
+                sets.push(new_set);
+            }
+            _ => {}
+        };
+        if sets.len() == 1 && sets[0].len() == num_points {
+            last_join = Some((a.clone(), b.clone()));
+            break;
+        }
+    }
+    last_join.unwrap().0.x * last_join.unwrap().1.x
 }
 
 fn main() {
     part1_test!(10, 40);
     part1_answer!(1000, 66640);
-    //     part2_test!(0);
-    //     part2_answer!(0);
+    part2_test!(25272);
+    part2_answer!(78894156);
 }
 
 #[test]
