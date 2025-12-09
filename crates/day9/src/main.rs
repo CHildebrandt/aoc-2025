17c3594889542571c26bbe666a2e230b0aca4205
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2025-12-09 17:24:01 +0100

    refactor day9

--- crates/day9/src/main.rs
+++ crates/day9/src/main.rs
@@ -18,20 +18,34 @@ fn part1(input: &str) -> usize {
 }
 
 enum Edge {
-    Vertical {
-        x: usize,
-        y_start: usize,
-        y_end: usize,
-    },
-    Horizontal {
-        y: usize,
-        x_start: usize,
-        x_end: usize,
-    },
+    Vertical { x: usize, start: usize, end: usize },
+    Horizontal { y: usize, start: usize, end: usize },
+}
+
+impl Edge {
+    fn intersects_rect(&self, l: usize, t: usize, r: usize, b: usize) -> bool {
+        match self {
+            Edge::Vertical { x, start, end } => {
+                if *x > l && *x < r {
+                    if (start < &t && end > &t) || (start < &b && end > &b) {
+                        return true;
+                    }
+                }
+                false
+            }
+            Edge::Horizontal { y, start, end } => {
+                if *y > t && *y < b {
+                    if (start < &l && end > &l) || (start < &r && end > &r) {
+                        return true;
+                    }
+                }
+                false
+            }
+        }
+    }
 }
 
 fn part2(input: &str) -> usize {
-    let mut edges = Vec::new();
     let combinations = input
         .lines()
         .map(|line| {
@@ -44,51 +58,40 @@ fn part2(input: &str) -> usize {
         .tuple_combinations()
         .collect::<Vec<((usize, usize), (usize, usize))>>();
 
-    for (a, b) in combinations.iter() {
-        if a.0 == b.0 {
-            edges.push(Edge::Vertical {
-                x: a.0,
-                y_start: a.1.min(b.1),
-                y_end: a.1.max(b.1),
-            });
-        } else if a.1 == b.1 {
-            edges.push(Edge::Horizontal {
-                y: a.1,
-                x_start: a.0.min(b.0),
-                x_end: a.0.max(b.0),
-            });
-        }
-    }
+    let edges = combinations
+        .iter()
+        .filter_map(|(a, b)| {
+            if a.0 == b.0 {
+                Some(Edge::Vertical {
+                    x: a.0,
+                    start: a.1.min(b.1),
+                    end: a.1.max(b.1),
+                })
+            } else if a.1 == b.1 {
+                Some(Edge::Horizontal {
+                    y: a.1,
+                    start: a.0.min(b.0),
+                    end: a.0.max(b.0),
+                })
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
 
     combinations
         .iter()
-        .filter(|(a, b)| {
-            // Smaller rectangle inside
-            let l = a.0.min(b.0) + 1;
-            let t = a.1.min(b.1) + 1;
-            let r = a.0.max(b.0) - 1;
-            let b = a.1.max(b.1) - 1;
-            // Check intersection with any edge
-            !edges.iter().any(|edge| match edge {
-                Edge::Vertical { x, y_start, y_end } => {
-                    if *x >= l && *x <= r {
-                        if (y_start <= &t && y_end >= &t) || (y_start <= &b && y_end >= &b) {
-                            return true;
-                        }
-                    }
-                    false
-                }
-                Edge::Horizontal { y, x_start, x_end } => {
-                    if *y >= t && *y <= b {
-                        if (x_start <= &l && x_end >= &l) || (x_start <= &r && x_end >= &r) {
-                            return true;
-                        }
-                    }
-                    false
-                }
-            })
+        .filter_map(|(a, b)| {
+            let left = a.0.min(b.0);
+            let top = a.1.min(b.1);
+            let right = a.0.max(b.0);
+            let bottom = a.1.max(b.1);
+            let area = ((right - left) + 1) * ((bottom - top) + 1);
+            (!edges
+                .iter()
+                .any(|edge| edge.intersects_rect(left + 1, top + 1, right - 1, bottom - 1)))
+            .then_some(area)
         })
-        .map(|(a, b)| ((a.0.max(b.0) - a.0.min(b.0)) + 1) * ((a.1.max(b.1) - a.1.min(b.1)) + 1))
         .max()
         .unwrap()
 }
